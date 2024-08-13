a7c20cb3102aab442a63f21e2bb3f052e0f413f0
iomyaki <io.mknkv@gmail.com>
2024-08-13 17:05:14 +0700

    utilities

--- utilities.py
+++ utilities.py
@@ -4,3 +4,8 @@ import sys
 sys.stdin.readline()  # instead of input()
 sys.stdout.write('sample text')  # instead of print('sample text')  # it is actually slower than print()
 sys.setrecursionlimit(10 ** 9)
+
+x = 3.1415
+x = int(x + 0.5)  # mathematically correct rounding
+
+print(f"{x:.12f}")  # print with 12 decimal places
